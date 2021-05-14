cf9de32a5c639286228a42ad5f40a3996e6d3d96
Shivam Maralay <shivam201@gmail.com>
2021-05-14 06:53:21 +0530

    Add files via upload

---
+++ src/main/java/srcpack/Invoker.java
@@ -0,0 +1,11 @@
+package srcpack;
+
+public class Invoker {
+
+	public static void main(String[] args) {
+		display obj = new display();
+		obj.displaystring();
+
+	}
+
+}
---
+++ src/main/java/srcpack/display.java
@@ -0,0 +1,9 @@
+package srcpack;
+
+public class display {
+	public String displaystring()
+	{
+		System.out.println("**** default display ****");
+		return "*** Returning display *** ";
+	}
+}
